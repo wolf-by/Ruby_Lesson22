bd33796d74d7f58e607d3011ad3b2ee1e4207cc4
wolf-by <wolf.by@yandex.ru>
2023-04-17 18:23:01 +0300

    Add Form end record txt file

--- app.rb
+++ app.rb
@@ -18,3 +18,27 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@master = params[:value]
+	@datetime = params[:datetime]
+
+	f = File.open './public/users.txt', 'a'
+	f.write "Клиент: #{@username}, Телефон: #{@phone}, Парикмахер: #{@master}, Дата и время: #{@datetime}\n"
+	f.close
+
+	erb :visit
+end
+
+post '/contacts' do
+	@email = params[:email]
+	@message = params[:message]
+	
+	f = File.open './public/contacts.txt', 'a'
+	f.write "Email клиент: #{@email}, Сообщение: #{@message}\n"
+	f.close
+
+	erb :contacts
+end
